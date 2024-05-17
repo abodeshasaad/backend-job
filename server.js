dfde86567192f47d225092be4124c07672015a70
Abd Alilah Alsheekh Asaad <Abdalilahalsheekhasaad@gmail.com>
2024-05-17 21:26:24 +0300

    new

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./lib/passportConfig");
+const nodemailer = require("nodemailer");
+const { v4: uuidv4 } = require("uuid");
+var cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
+// // MongoDsB
+// mongoose
+//   .connect("mongodb://localhost:27017/jobPortal", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((res) => console.log("Connected to DB.."))
+//   .catch((err) => console.log(err));
+
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// initialising directories
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
+}
+
+const app = express();
+const port = process.env.PORT || 4444;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+//static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
+// Routing
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+app.use("/host", require("./routes/downloadRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
